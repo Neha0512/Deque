3862a38b93619c3bec60c9d219e4069ee22892a5
Neha0512 <78rakes@gmail.com>
2022-03-09 19:59:24 +0530

    updated

--- implementation.cpp
+++ implementation.cpp
@@ -1,7 +1,15 @@
 // inserting an element in deque take O(n) time
-
+// front should always be at 0
 
 #include<bits/stdc++.h>
 using namespace std;
 
-
+struct dq{
+	int size, capacity;
+	int *arr;
+	dq(int c){
+		capacity=c;
+		size=0;
+		arr=new int[cap];
+	}
+};
